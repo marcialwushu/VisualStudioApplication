e17ec5401461dff18f5ebca935c16f66ba36ec7f
cleilson pereira <cleilson pereira>
2017-05-11 01:46:45 -0300

    CRUD ASP.NET - utilizando o mesmo database da Web API - funconando

---
+++ Projects/CRUDAgendaContato/CRUDAgendaContato/Controllers/ContatosController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CRUDAgendaContato.Models;
+
+namespace CRUDAgendaContato.Controllers
+{
+    public class ContatosController : Controller
+    {
+        private AgendaContatoEntities db = new AgendaContatoEntities();
+
+        // GET: Contatos
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Contato.ToListAsync());
+        }
+
+        // GET: Contatos/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contato contato = await db.Contato.FindAsync(id);
+            if (contato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contato);
+        }
+
+        // GET: Contatos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contatos/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ContatoID,FirstName,LastName,Phone")] Contato contato)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contato.Add(contato);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(contato);
+        }
+
+        // GET: Contatos/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contato contato = await db.Contato.FindAsync(id);
+            if (contato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contato);
+        }
+
+        // POST: Contatos/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ContatoID,FirstName,LastName,Phone")] Contato contato)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contato).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(contato);
+        }
+
+        // GET: Contatos/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contato contato = await db.Contato.FindAsync(id);
+            if (contato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contato);
+        }
+
+        // POST: Contatos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Contato contato = await db.Contato.FindAsync(id);
+            db.Contato.Remove(contato);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
